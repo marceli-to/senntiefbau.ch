a1858b63eda5ab629a0726e03a2dfca3e2aeafbe
Marcel Stadelmann <m@marceli.to>
2026-02-25 10:38:39 +0100

    Add imprint, privacy

--- resources/views/imprint.blade.php
+++ resources/views/imprint.blade.php
@@ -1,24 +1,58 @@
 <x-layout.head />
 <x-layout.body>
   <main class="flex-1 bg-sand">
-    <div class=" px-40 pt-30 lg:pt-60">
-
+    <div class="px-40 py-30 lg:py-50">
       <x-layout.container>
 
         <div class="flex flex-col gap-y-20 md:grid md:grid-cols-12 md:gap-x-40">
           <div class="md:col-span-8">
-            <x-headings.h1 class="!text-teal text-left !px-0">
+            <x-headings.h1 class="!text-teal text-left !px-0 !mb-10 lg:!mb-20">
               Impressum
             </x-headings.h1>
 
             <div class="flex flex-col gap-y-20 lg:gap-y-30">
+
+              <div>
+                <x-headings.h2 class="!mb-5">
+                  Senn Tiefbau AG
+                </x-headings.h2>
+                <p>
+                  Schärlibachstrasse 10<br>
+                  8374 Dussnang<br>
+                  <a href="tel:+41719771000">071 977 10 00</a><br>
+                  <a href="mailto:info@senntiefbau.ch">info@senntiefbau.ch</a>
+                </p>
+              </div>
+
+              <div>
+                <x-headings.h2 class="!mb-5">
+                  Design und Entwicklung
+                </x-headings.h2>
+                <p>
+                  Stoz Werbeagentur AG<br>
+                  Barzloostrasse 2<br>
+                  8330 Pfäffikon ZH<br>
+                  <a href="mailto:hello@stoz.ch">hello@stoz.ch</a><br>
+                  <a href="https://www.stoz.ch" target="_blank">www.stoz.ch</a>
+                </p>
+              </div>
+
+              <div>
+                <x-headings.h2 class="!mb-5">
+                  Programmierung
+                </x-headings.h2>
+                <p>
+                  Marcel Stadelmann, Zürich<br>
+                  <a href="https://marceli.to" target="_blank">marceli.to</a>
+                </p>
+              </div>
+
             </div>
 
           </div>
 
         </div>
       </x-layout.container>
-
     </div>
   </main>
   <x-layout.footer />
--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -1,56 +1,79 @@
 <x-layout.head />
 <x-layout.body>
-  <main class="flex-1 pt-40 lg:pt-60">
-    <x-layout.container>
-      <div class="flex flex-col gap-y-20 md:grid md:grid-cols-12 md:gap-x-40 pb-20 lg:pb-60">
-        <div class="md:col-span-8">
-          <x-headings.h1 class="!text-teal text-left !px-0 !mb-10 lg:!mb-20">
-            Datenschutz
-          </x-headings.h1>
-          <div class="flex flex-col gap-y-20 lg:gap-y-30">
-            <div>
-              <strong>Verantwortliche Stelle</strong>
-              <p class="mt-5">
-                Senn Tiefbau AG<br>
-                Schärlibachstrasse 10<br>
-                8374 Dussnang<br>
-                <a href="mailto:info@senntiefbau.ch">info@senntiefbau.ch</a>
-              </p>
-            </div>
-            <div>
-              <strong>Erhebung und Verwendung von Daten</strong>
-              <p class="mt-5">
-                Beim Besuch unserer Website werden automatisch technische Daten erfasst (z.&nbsp;B. IP-Adresse, Browsertyp, Zugriffszeit). Diese Daten werden ausschliesslich zur Sicherstellung eines reibungslosen Betriebs der Website verwendet und nicht mit Ihrer Person in Verbindung gebracht.
-              </p>
-            </div>
-            <div>
-              <strong>Kontaktaufnahme</strong>
-              <p class="mt-5">
-                Wenn Sie uns per E-Mail oder Telefon kontaktieren, werden Ihre Angaben zur Bearbeitung Ihrer Anfrage gespeichert. Diese Daten werden ohne Ihre Einwilligung nicht an Dritte weitergegeben.
-              </p>
-            </div>
-            <div>
-              <strong>Cookies</strong>
-              <p class="mt-5">
-                Unsere Website verwendet keine Tracking-Cookies. Es werden lediglich technisch notwendige Cookies eingesetzt, die für den Betrieb der Website erforderlich sind.
-              </p>
-            </div>
-            <div>
-              <strong>Ihre Rechte</strong>
-              <p class="mt-5">
-                Sie haben das Recht, Auskunft über Ihre gespeicherten Daten zu erhalten, diese berichtigen oder löschen zu lassen. Wenden Sie sich dazu an die oben genannte verantwortliche Stelle.
-              </p>
-            </div>
-            <div>
-              <strong>Änderungen</strong>
-              <p class="mt-5">
-                Wir behalten uns vor, diese Datenschutzerklärung bei Bedarf anzupassen. Es gilt jeweils die aktuelle, auf unserer Website veröffentlichte Fassung.
-              </p>
+  <main class="flex-1 bg-sand">
+    <div class="px-40 py-30 lg:py-50">
+      <x-layout.container>
+        <div class="flex flex-col gap-y-20 md:grid md:grid-cols-12 md:gap-x-40 pb-20 lg:pb-60">
+          <div class="md:col-span-8">
+            <x-headings.h1 class="!text-teal text-left !px-0 !mb-10 lg:!mb-20">
+              Datenschutz
+            </x-headings.h1>
+            <div class="flex flex-col gap-y-20 lg:gap-y-30">
+
+              <div>
+                <p>Die Betreiber dieser Seiten nehmen den Schutz Ihrer persönlichen Daten sehr ernst. Wir behandeln Ihre personenbezogenen Daten vertraulich und entsprechend der gesetzlichen Datenschutzvorschriften sowie dieser Datenschutzerklärung.</p>
+              </div>
+              <div>
+                <x-headings.h2 class="!mb-5">
+                  Cookies
+                </x-headings.h2>
+                <p>Die Internetseiten verwenden teilweise so genannte Cookies. Cookies richten auf Ihrem Rechner keinen Schaden an und enthalten keine Viren. Cookies dienen dazu, unser Angebot nutzerfreundlicher, effektiver und sicherer zu machen. Cookies sind kleine Textdateien, die auf Ihrem Rechner abgelegt werden und die Ihr Browser speichert.</p>
+                <p>Die meisten der von uns verwendeten Cookies sind so genannte „Session-Cookies". Sie werden nach Ende Ihres Besuchs automatisch gelöscht. Andere Cookies bleiben auf Ihrem Endgerät gespeichert, bis Sie diese löschen. Diese Cookies ermöglichen es uns, Ihren Browser beim nächsten Besuch wiederzuerkennen.</p>
+                <p>Sie können Ihren Browser so einstellen, dass Sie über das Setzen von Cookies informiert werden und Cookies nur im Einzelfall erlauben, die Annahme von Cookies für bestimmte Fälle oder generell ausschliessen sowie das automatische Löschen der Cookies beim Schliessen des Browser aktivieren. Bei der Deaktivierung von Cookies kann die Funktionalität dieser Website eingeschränkt sein.</p>
+              </div>
+              <div>
+                <x-headings.h2 class="!mb-5">
+                  Hosting Provider &amp; Server-LogFiles
+                </x-headings.h2>
+                <p>Der Provider der Seiten erhebt und speichert automatisch Informationen in so genannten Server-Log Files, die Ihr Browser automatisch an uns übermittelt. Dies sind:</p>
+                <ul class="list-disc pl-20">
+                  <li>IP-Adresse</li>
+                  <li>Browsertyp und Browserversion</li>
+                  <li>verwendetes Betriebssystem</li>
+                  <li>Referrer URL</li>
+                  <li>Hostname des zugreifenden Rechners</li>
+                  <li>Uhrzeit der Serveranfrage</li>
+                </ul>
+                <p>Diese Daten können nicht direkt bestimmten Personen zugeordnet werden. Eine Zusammenführung dieser Daten mit anderen Datenquellen wird nicht vorgenommen. Wir behalten uns vor, diese Daten nachträglich zu prüfen, wenn uns konkrete Anhaltspunkte für eine rechtswidrige Nutzung bekannt werden.</p>
+                <p>Diese Daten sowie alle Daten dieser Website werden bei unserem Hosting-Provider webkeeper GmbH, 8604 Volketswil, Schweiz gespeichert, deren Datenschutzerklärung Sie <a href="https://www.webkeeper.ch/de/unternehmen/datenschutz" target="_blank" class="underline underline-offset-4 decoration-1 hover:no-underline">hier</a> finden.</p>
+              </div>
+              <div>
+                <x-headings.h2 class="!mb-5">
+                  Kontaktformular
+                </x-headings.h2>
+                <x-headings.h3 class="!mb-5">Umfang, Zweck und Rechtsgrundlage der Datenverarbeitung</x-headings.h3>
+                <p>Auf unserer Internetseite ist ein Kontaktformular vorhanden, welches für die elektronische Kontaktaufnahme genutzt werden kann. Nehmen Sie diese Möglichkeit wahr, so werden die in der Eingabemaske eingegeben Daten an uns übermittelt und gespeichert. Diese Daten sind:</p>
+                <ul class="list-disc pl-20">
+                  <li>Name und Vorname sowie Anrede</li>
+                  <li>Firmenname, sofern angegeben</li>
+                  <li>Telefonnummer</li>
+                  <li>E-Mailadresse</li>
+                  <li>Adresse, sofern angegeben</li>
+                  <li>Nachricht, welche Sie uns übermitteln wollen</li>
+                </ul>
+                <p>Im Zeitpunkt der Absendung der Nachricht werden zudem folgende Daten gespeichert:</p>
+                <ul class="list-disc pl-20">
+                  <li>Die IP-Adresse des Nutzers</li>
+                  <li>Datum und Uhrzeit der Registrierung</li>
+                </ul>
+                <p>Für die Verarbeitung der Daten wird im Rahmen des Absendevorgangs Ihre Einwilligung eingeholt und auf diese Datenschutzerklärung verwiesen.</p>
+                <p>Die Verarbeitung der personenbezogenen Daten aus der Eingabemaske dient uns allein zur Bearbeitung der Kontaktaufnahme. Die sonstigen während des Absendevorgangs verarbeiteten personenbezogenen Daten dienen dazu, einen Missbrauch des Kontaktformulars zu verhindern und die Sicherheit unserer informationstechnischen Systeme sicherzustellen.</p>
+                <x-headings.h3 class="!mb-5">Dauer der Speicherung und Widerspruchsrecht</x-headings.h3>
+                <p>Sie haben jederzeit die Möglichkeit, Ihre Einwilligung zur Verarbeitung der personenbezogenen Daten per E-Mail an <a href="mailto:info@senntiefbau.ch" class="underline underline-offset-4 decoration-1 hover:no-underline">info@senntiefbau.ch</a> zu widerrufen. In einem solchen Fall kann die Konversation nicht fortgeführt werden.</p>
+                <p>Alle personenbezogenen Daten, die im Zuge der Kontaktaufnahme gespeichert wurden, werden in diesem Fall gelöscht.</p>
+              </div>
+              <div>
+                <x-headings.h2 class="!mb-5">
+                  Recht auf Auskunft, Löschung, Sperrung
+                </x-headings.h2>
+                <p>Sie haben jederzeit das Recht auf unentgeltliche Auskunft über Ihre gespeicherten personenbezogenen Daten, deren Herkunft und Empfänger und den Zweck der Datenverarbeitung sowie ein Recht auf Berichtigung, Sperrung oder Löschung dieser Daten. Hierzu sowie zu weiteren Fragen zum Thema personenbezogene Daten können Sie sich jederzeit unter der im Impressum angegebenen Adresse an uns wenden oder eine E-Mail an <a href="mailto:info@senntiefbau.ch" class="underline underline-offset-4 decoration-1 hover:no-underline">info@senntiefbau.ch</a> senden.</p>
+              </div>
+    
             </div>
           </div>
         </div>
-      </div>
-    </x-layout.container>
+      </x-layout.container>
+    </div>
   </main>
   <x-layout.footer />
 </x-layout.body>
